dfb8ca1e49c600d230e0110c16eba3f413df87f4
Dylan Scott <dylanrhysscott@gmail.com>
2021-10-24 21:04:18 +0100

    Scaffolded VCS API

--- api/vcs/vcs.go
+++ api/vcs/vcs.go
@@ -1,6 +1,10 @@
 package vcs
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/dylanrhysscott/terrarium/pkg/types"
+)
 
 type VCSAPIInterface interface {
 	CreateVCSHandler() http.Handler
@@ -11,4 +15,54 @@ type VCSAPIInterface interface {
 }
 
 type VCSAPI struct {
+	OrganziationStore types.OrganizationStore
+	ErrorHandler      types.APIErrorWriter
+	ResponseHandler   types.APIResponseWriter
+	VCSStore          types.VCSStore
+}
+
+// CreateVCSHandler is a handler for creating an organization VCS connection (POST)
+func (v *VCSAPI) CreateVCSHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+// UpdateVCSHandler is a handler for updating an organization VCS connection (PUT)
+func (v *VCSAPI) UpdateVCSHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+// GetVCSHandler is a handler for getting a single organization VCS connection (GET)
+func (v *VCSAPI) GetVCSHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+// ListVCSHandler is a handler for listing all organization VCS connections (GET)
+func (v *VCSAPI) ListVCSsHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+// DeleteVCSHandler is a handler for deleting an organization VCS connection (DELETE)
+func (v *VCSAPI) DeleteVCSHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+// NewVCSAPI creates an instance of the VCS API with the reqired database
+// driver support
+func NewVCSAPI(vcsstore types.VCSStore, orgstore types.OrganizationStore, responseHandler types.APIResponseWriter, errorHandler types.APIErrorWriter) *VCSAPI {
+	return &VCSAPI{
+		OrganziationStore: orgstore,
+		VCSStore:          vcsstore,
+		ResponseHandler:   responseHandler,
+		ErrorHandler:      errorHandler,
+	}
 }
--- internal/terrariummongo/vcs.go
+++ internal/terrariummongo/vcs.go
@@ -13,32 +13,38 @@ type VCSBackend struct {
 }
 
 // Init initializes the VCS table
-func (o *VCSBackend) Init() error {
+func (v *VCSBackend) Init() error {
 	return nil
 }
 
 // Create Adds a new VCS to the VCS table
-func (o *VCSBackend) Create(name string, orgID string, serviceProvider string, httpURI string, apiURI string, clientID string, clientSecret string, callback string) (*types.VCS, error) {
-
+func (v *VCSBackend) Create(name string, orgID string, serviceProvider string, httpURI string, apiURI string, clientID string, clientSecret string, callback string) (*types.VCS, error) {
 	return nil, nil
 }
 
 // ReadAll Returns all VCSs from the VCS table
-func (o *VCSBackend) ReadAll(limit int, offset int) ([]*types.VCS, error) {
+func (v *VCSBackend) ReadAll(limit int, offset int) ([]*types.VCS, error) {
+	// ctx := context.TODO()
+	// limitOpt := options.Find().SetLimit(int64(limit))
+	// skipOpt := options.Find().SetSkip(int64(offset))
+	// cur, err := v.client.Database(v.Database).Collection(v.CollectionName).Find(ctx, bson.D{}, limitOpt, skipOpt)
+	// if err != nil {
+	// 	return nil, err
+	// }
 	return nil, nil
 }
 
 // ReadOne Returns a single VCS from the VCSs table
-func (o *VCSBackend) ReadOne(orgName string) (*types.VCS, error) {
+func (v *VCSBackend) ReadOne(orgName string) (*types.VCS, error) {
 	return nil, nil
 }
 
 // Update Updates an VCS in the VCS table
-func (o *VCSBackend) Update(name string, orgID string, serviceProvider string, httpURI string, apiURI string, clientID string, clientSecret string, callback string) (*types.VCS, error) {
+func (v *VCSBackend) Update(name string, orgID string, serviceProvider string, httpURI string, apiURI string, clientID string, clientSecret string, callback string) (*types.VCS, error) {
 	return nil, nil
 }
 
 // Delete Removes an VCS from the VCS table
-func (o *VCSBackend) Delete(name string) error {
+func (v *VCSBackend) Delete(name string) error {
 	return nil
 }
