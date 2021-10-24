20cc1d5738960ba74e67147157dcf25e5694c8fe
Dylan Scott <dylanrhysscott@gmail.com>
2021-10-24 21:14:52 +0100

    Initaise VCS Routes

--- api/api.go
+++ api/api.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/dylanrhysscott/terrarium/api/organization"
+	"github.com/dylanrhysscott/terrarium/api/vcs"
 	"github.com/dylanrhysscott/terrarium/pkg/types"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -32,6 +33,7 @@ func (t *Terrarium) Serve() error {
 // setupOrganizationRoutes configures the organization API subrouter
 func (t *Terrarium) setupOrganizationRoutes(path string) {
 	apiHandlers := organization.NewOrganizationAPI(t.Store.Organizations(), t.Responder, t.Errorer)
+	vcsHandlers := vcs.NewVCSAPI(t.Store.VCS(), t.Store.Organizations(), t.Responder, t.Errorer)
 	s := t.Router.PathPrefix(path).Subrouter()
 	s.StrictSlash(true)
 	s.Handle("/", apiHandlers.ListOrganizationsHandler()).Methods(http.MethodGet)
@@ -39,6 +41,18 @@ func (t *Terrarium) setupOrganizationRoutes(path string) {
 	s.Handle("/{organization_name}", apiHandlers.GetOrganizationHandler()).Methods(http.MethodGet)
 	s.Handle("/{organization_name}", apiHandlers.UpdateOrganizationHandler()).Methods(http.MethodPatch)
 	s.Handle("/{organization_name}", apiHandlers.DeleteOrganizationHandler()).Methods(http.MethodDelete)
+	s.Handle("/{organization_name}/oauth-clients", vcsHandlers.ListVCSsHandler()).Methods(http.MethodGet)
+	s.Handle("/{organization_name}/oauth-clients", vcsHandlers.CreateVCSHandler()).Methods(http.MethodPost)
+}
+
+// setupOAuthClientRoutes configures the VCS API subrouter
+func (t *Terrarium) setupOAuthClientRoutes(path string) {
+	vcsHandlers := vcs.NewVCSAPI(t.Store.VCS(), t.Store.Organizations(), t.Responder, t.Errorer)
+	s := t.Router.PathPrefix(path).Subrouter()
+	s.StrictSlash(true)
+	s.Handle("/{id}", vcsHandlers.GetVCSHandler()).Methods(http.MethodGet)
+	s.Handle("/{id}", vcsHandlers.UpdateVCSHandler()).Methods(http.MethodPatch)
+	s.Handle("/{id}", vcsHandlers.DeleteVCSHandler()).Methods(http.MethodDelete)
 }
 
 // NewTerrarium creates a new Terrarium instance setting up the required API routes
@@ -51,5 +65,6 @@ func NewTerrarium(port int, driver types.TerrariumDriver, responder types.APIRes
 		Errorer:   errorer,
 	}
 	t.setupOrganizationRoutes("/v1/organizations")
+	t.setupOAuthClientRoutes("/v1/oauth-clients")
 	return t
 }
