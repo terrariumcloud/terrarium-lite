794e4ad6e6cc9e11d5fbf51ec21d766892ef695c
Dylan Scott <dylanrhysscott@gmail.com>
2021-10-29 15:23:50 +0100

    Rename modules API to sources for VCS integration

---
+++ internal/terrariummongo/sources.go
@@ -0,0 +1,8 @@
+package terrariummongo
+
+// SourceBackend is a struct that implements source interactions for module documents
+type SourceBackend struct{}
+
+func (s *SourceBackend) FetchVCSSources() {
+
+}
