802add6265d006107af7460742767b1bc12f96ca
Dylan Scott <dylanrhysscott@gmail.com>
2021-12-13 16:53:52 +0000

    Endpoint Setup

--- api/modules.go
+++ api/modules.go
@@ -9,24 +9,92 @@ import (
 
 // ModuleAPIInterface specifies the required HTTP handlers for a Terrarium Modules API
 type ModuleAPIInterface interface {
-	CreateOrganizationHandler() http.Handler
-	GetOrganizationHandler() http.Handler
-	UpdateOrganizationHandler() http.Handler
-	ListOrganizationsHandler() http.Handler
-	DeleteOrganizationHandler() http.Handler
+	CreateModuleHandler() http.Handler
+	GetModuleHandler() http.Handler
+	GetModuleVersionHandler() http.Handler
+	DownloadModuleHandler() http.Handler
+	UpdateModuleHandler() http.Handler
+	ListModulesHandler() http.Handler
+	ListOrganizationModulesHandler() http.Handler
+	DeleteModuleHandler() http.Handler
 }
 
 // ModuleAPI is a struct implementing the handlers for the Module API in Terrarium
+// TODO: Implement Module Store
 type ModuleAPI struct {
 	Router          *mux.Router
-	ModuleStore     types.OrganizationStore
+	ModuleStore     interface{}
 	ErrorHandler    types.APIErrorWriter
 	ResponseHandler types.APIResponseWriter
 }
 
-func NewModulesAPI(router *mux.Router, path string, store types.OrganizationStore, responseHandler types.APIResponseWriter, errorHandler types.APIErrorWriter) *ModuleAPI {
-	return &ModuleAPI{
-		Router:      router,
-		ModuleStore: nil,
+func (m *ModuleAPI) CreateModuleHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) GetModuleHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) DownloadModuleHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) GetModuleVersionHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) UpdateModuleHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) ListModulesHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) ListOrganizationModulesHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) DeleteModuleHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (m *ModuleAPI) SetupRoutes() {
+	m.Router.StrictSlash(true)
+	m.Router.Handle("/", m.ListModulesHandler()).Methods(http.MethodGet)
+	m.Router.Handle("/{organization_name}", m.ListOrganizationModulesHandler()).Methods(http.MethodGet)
+	m.Router.Handle("/", m.CreateModuleHandler()).Methods(http.MethodPost)
+	m.Router.Handle("/{organization_name}/{name}/{provider}", m.GetModuleHandler()).Methods(http.MethodGet)
+	m.Router.Handle("/{organization_name}/{name}/{provider}/versions", m.GetModuleVersionHandler()).Methods(http.MethodGet)
+	m.Router.Handle("/{organization_name}/{name}/{provider}/{version}/download", m.DownloadModuleHandler()).Methods(http.MethodGet)
+	m.Router.Handle("/{organization_name}/{name}/{provider}", m.UpdateModuleHandler()).Methods(http.MethodPatch)
+	m.Router.Handle("/{organization_name}/{name}/{provider}", m.DeleteModuleHandler()).Methods(http.MethodDelete)
+}
+
+func NewModuleAPI(router *mux.Router, path string, store interface{}, responseHandler types.APIResponseWriter, errorHandler types.APIErrorWriter) *ModuleAPI {
+	m := &ModuleAPI{
+		Router:          router,
+		ModuleStore:     store,
+		ErrorHandler:    errorHandler,
+		ResponseHandler: responseHandler,
 	}
+	m.SetupRoutes()
+	return m
 }
