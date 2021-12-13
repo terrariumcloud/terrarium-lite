02442c92af257c446a4846f8f48f768fa0cbeba2
Dylan Scott <dylanrhysscott@gmail.com>
2021-12-13 20:58:07 +0000

    Correctly return header to downloadable zip location

--- api/modules.go
+++ api/modules.go
@@ -17,6 +17,7 @@ type ModuleAPIInterface interface {
 	GetModuleHandler() http.Handler
 	GetModuleVersionHandler() http.Handler
 	DownloadModuleHandler() http.Handler
+	ArchiveHandler() http.Handler
 	UpdateModuleHandler() http.Handler
 	ListModulesHandler() http.Handler
 	ListOrganizationModulesHandler() http.Handler
@@ -47,14 +48,8 @@ func (m *ModuleAPI) GetModuleHandler() http.Handler {
 
 func (m *ModuleAPI) DownloadModuleHandler() http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		zipData, err := m.FileStore.FetchModuleSource(context.TODO(), "terrarium-dev", "test.zip")
-		if err != nil {
-			log.Printf("[FILE STORE] Error: %s", err.Error())
-			m.ErrorHandler.Write(rw, errors.New("failed fetching module source from file store"), http.StatusInternalServerError)
-			return
-		}
-		r.Header.Set("Content-Type", "application/zip")
-		rw.Write(zipData)
+		rw.Header().Add("X-Terraform-Get", "../archive")
+		m.ResponseHandler.Write(rw, nil, http.StatusNoContent)
 	})
 }
 
@@ -101,6 +96,19 @@ func (m *ModuleAPI) DeleteModuleHandler() http.Handler {
 	})
 }
 
+func (m *ModuleAPI) ArchiveHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		zipData, err := m.FileStore.FetchModuleSource(context.TODO(), "terrarium-dev", "test.zip")
+		if err != nil {
+			log.Printf("[FILE STORE] Error: %s", err.Error())
+			m.ErrorHandler.Write(rw, errors.New("failed fetching module source from file store"), http.StatusInternalServerError)
+			return
+		}
+		r.Header.Set("Content-Type", "application/zip")
+		rw.Write(zipData)
+	})
+}
+
 func (m *ModuleAPI) SetupRoutes() {
 	m.Router.StrictSlash(true)
 	m.Router.Handle("/", m.ListModulesHandler()).Methods(http.MethodGet)
@@ -111,6 +119,8 @@ func (m *ModuleAPI) SetupRoutes() {
 	m.Router.Handle("/{organization_name}/{name}/{provider}/{version}/download", m.DownloadModuleHandler()).Methods(http.MethodGet)
 	m.Router.Handle("/{organization_name}/{name}/{provider}", m.UpdateModuleHandler()).Methods(http.MethodPatch)
 	m.Router.Handle("/{organization_name}/{name}/{provider}", m.DeleteModuleHandler()).Methods(http.MethodDelete)
+	m.Router.Handle("/{organization_name}/{name}/{provider}/{version}/archive", m.ArchiveHandler()).Methods(http.MethodGet)
+
 }
 
 func NewModuleAPI(router *mux.Router, path string, store interface{}, fileStore types.TerrariumStorageDriver, responseHandler types.APIResponseWriter, errorHandler types.APIErrorWriter) *ModuleAPI {
