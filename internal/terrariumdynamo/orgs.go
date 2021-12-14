3be1780613431d508c0959c036c345e1ae2d387b
Dylan Scott <dylanrhysscott@gmail.com>
2021-12-14 16:07:59 +0000

    Added single org query

--- internal/terrariumdynamo/orgs.go
+++ internal/terrariumdynamo/orgs.go
@@ -140,9 +140,12 @@ func (o *OrganizationBackend) ReadAll(limit int, offset int) ([]*types.Organizat
 func (o *OrganizationBackend) ReadOne(orgName string) (*types.Organization, error) {
 	ctx := context.TODO()
 	org, err := o.Client.Query(ctx, &dynamodb.QueryInput{
-		KeyConditionExpression: aws.String(fmt.Sprintf("name = %s", orgName)),
+		KeyConditionExpression: aws.String(fmt.Sprintf("#n = %s", orgName)),
 		Limit:                  aws.Int32(int32(1)),
 		TableName:              &o.TableName,
+		ExpressionAttributeNames: map[string]string{
+			"#n": orgName
+		},
 	})
 	if err != nil {
 		return nil, err
