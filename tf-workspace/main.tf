1dd8503892fadb8c92fe74056bbe6ed55622c8fc
Dylan Scott <dylanrhysscott@gmail.com>
2021-12-16 13:36:33 +0000

    Working dynamic registry implementation

--- tf-workspace/main.tf
+++ tf-workspace/main.tf
@@ -1,9 +1,19 @@
-module "test" {
-    source = "api.terrarium.cloud/test/test/aws"
-    version = "0.0.1"
-}
+module "vpc" {
+  source  = "api.terrarium.cloud/dylanrhysscott/terraform-aws-vpc/aws"
+  version = "3.11.0"
+  name    = "my-vpc"
+  cidr    = "10.0.0.0/16"
 
-module "test2" {
-    source = "api.terrarium.cloud/nonexistent/test/aws"
-    version = "0.0.1"
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
 }
